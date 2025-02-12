fc6c00144758f1bc3f1098dd6e272e2fe2817996
avadaliDevOps <adityavittal92@gmail.com>
2025-02-12 12:18:24 +0530

    update and push

---
+++ sample.sh
@@ -0,0 +1,13 @@
+echo "this is the first line"
+echo "this is the second line"
+
+# this is a comment
+
+echo "this is the third line"
+
+# this is a comment
+
+echo "this is the fourth line"
+
+# this is a comment
+pwd
